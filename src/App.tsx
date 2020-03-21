9d6391a44e68eee15ff1a3d8a1e5df917b884a35
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-03-21 14:52:09 +0100

    Add card route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import {HashRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Link, Route, Switch, useParams} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
+function Card() {
+  let { cardNumber } = useParams();
+
+  return (<div className="App">
+    <header className="App-header">
+      <p>
+        Number: {cardNumber}
+      </p>
+    </header>
+  </div>);
+}
 
 function App() {
   return (
@@ -12,7 +24,10 @@ function App() {
               <Link to="/">Default</Link>
             </li>
             <li>
-              <Link to="/test">Test</Link>
+              <Link to="/card/1">Card 1</Link>
+            </li>
+            <li>
+              <Link to="/card/2">Card 2</Link>
             </li>
           </ul>
 
@@ -37,14 +52,8 @@ function App() {
                 </header>
               </div>
             </Route>
-            <Route path="/test">
-              <div className="App">
-                <header className="App-header">
-                  <p>
-                    Test!
-                  </p>
-                </header>
-              </div>
+            <Route path="/card/:cardNumber">
+              <Card/>
             </Route>
           </Switch>
         </div>
