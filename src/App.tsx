e6c9973611e05935bef6b60b95e5f7a39b36cbec
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-03-21 13:28:12 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
+import {HashRouter as Router, Link, Route, Switch} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. A test change!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Default</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+
+          <hr/>
+
+          <Switch>
+            <Route exact path="/">
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload. A test change!
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </Route>
+            <Route path="/test">
+              <div className="App">
+                <header className="App-header">
+                  <p>
+                    Test!
+                  </p>
+                </header>
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
