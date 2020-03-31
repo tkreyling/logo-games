d0d00826707b2c9482dfdc26995b81ff9e1ffccf
Thomas Kreyling <thomas.kreyling@googlemail.com>
2020-03-31 10:37:11 +0200

    Remove logo and styles from create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,28 @@ import React from 'react';
 import {HashRouter as Router, Link, Route, Switch, useParams} from "react-router-dom";
 import cat from './Noto_Emoji_Pie_1f408.svg';
 import couch from './Noto_Emoji_Pie_1f6cb.svg'
-import logo from './logo.svg';
 import './App.css';
 
 function Card() {
   let { cardNumber } = useParams();
 
-  return (<div className="App">
-    <header className="App-header">
+  return (
       <div className="card">
         <div className="card-inner">
           <div className="card-front">
             <h1>Number: {cardNumber}</h1>
           </div>
           <div className="card-back">
-            <div style={{ border: "3px solid #73AD21", position: "absolute", left: "10%", top: "15%", width: "80%" }}>
-              <img src={couch} alt="couch" style={{ width: "100%" }}/>
+            <div style={{border: "3px solid #73AD21", position: "absolute", left: "10%", top: "15%", width: "80%"}}>
+              <img src={couch} alt="couch" style={{width: "100%"}}/>
             </div>
-            <div style={{ border: "3px solid #73AD21", position: "absolute", left: "45%", top: "72%", width: "10%"}}>
-              <img src={cat} alt="cat" style={{ width: "100%" }} />
+            <div style={{border: "3px solid #73AD21", position: "absolute", left: "45%", top: "72%", width: "10%"}}>
+              <img src={cat} alt="cat" style={{width: "100%"}}/>
             </div>
           </div>
         </div>
       </div>
-    </header>
-  </div>);
+  );
 }
 
 function App() {
@@ -49,22 +46,9 @@ function App() {
 
           <Switch>
             <Route exact path="/">
-              <div className="App">
-                <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>
-                    Edit <code>src/App.tsx</code> and save to reload. A test change!
-                  </p>
-                  <a
-                      className="App-link"
-                      href="https://reactjs.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                  >
-                    Learn React
-                  </a>
-                </header>
-              </div>
+              <p>
+                Übung für Präpositionen
+              </p>
             </Route>
             <Route path="/card/:cardNumber">
               <Card/>
